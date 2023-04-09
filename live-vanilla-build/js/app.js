a0c6e19f7b8f091fdd87d9a68593677b1680e208
Rick Ahlgren <rick.ahlgren@gmail.com>
2023-04-09 09:16:52 -0400

    added alert if recent move results in tie or victory condition

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -1,6 +1,133 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelector(".items");
+// RESUME VIDEO AT 2:42:11
 
-menu.addEventListener("click", (event) => {
-  menuItems.classList.toggle("hidden");
-});
+const App = {
+  // All of our selected HTML elements
+  $: {
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]'),
+  },
+
+  state: {
+    moves: [],
+  },
+
+  getGameStatus(moves) {
+    const p1Moves = moves
+      .filter((move) => move.playerId === 1)
+      .map((move) => +move.squareId);
+    const p2Moves = moves
+      .filter((move) => move.playerId === 2)
+      .map((move) => +move.squareId);
+
+    console.log(p1Moves);
+
+    // Check if there is a winner or tie
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    winningPatterns.forEach((pattern) => {
+      const p1Wins = pattern.every((v) => p1Moves.includes(v));
+      const p2Wins = pattern.every((v) => p2Moves.includes(v));
+
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+
+    return {
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress",
+      winner, // 1 | 2 | null
+    };
+  },
+
+  init() {
+    App.registerEventListeners();
+  },
+
+  registerEventListeners() {
+    // DONE
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+
+    // TODO
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset the game");
+    });
+
+    // TODO
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("Start a new round");
+    });
+
+    // This event listener checks if a square has a play or not before 
+    // inserting the current player's play, then it adds the play to 
+    // that player's list (array) of plays, and then it checks to see 
+    // if that play resulted in a tie or victory for the current player
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        // Check if square has a play already, return early
+        const hasMove = (squareId) => {
+          const existingMove = App.state.moves.find(
+            (move) => move.squareId === squareId
+          );
+          return existingMove !== undefined;
+        };
+
+        if (hasMove(+square.id)) {
+          return;
+        }
+
+        // Determine which player icon to add to the square
+        const lastMove = App.state.moves.at(-1);
+        const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+        const currentPlayer =
+          App.state.moves.length === 0
+            ? 1
+            : getOppositePlayer(lastMove.playerId);
+
+        const icon = document.createElement("i");
+
+        if (currentPlayer === 1) {
+          icon.classList.add("fa-solid", "fa-x", "yellow");
+        } else {
+          icon.classList.add("fa-solid", "fa-o", "turquoise");
+        }
+
+        App.state.moves.push({
+          squareId: +square.id,
+          playerId: currentPlayer,
+        });
+
+        console.log(App.state);
+
+        square.replaceChildren(icon);
+
+        // Check if there is a winner or tie
+        const game = App.getGameStatus(App.state.moves);
+
+        if (game.status === "complete") {
+          if (game.winner) {
+            alert(`Player ${game.winner} wins!`);
+          } else {
+            alert("Tie!");
+          }
+        }
+      });
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
